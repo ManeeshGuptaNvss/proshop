4320193c1f7826e0efc3a63b276feb9157a89a8a
ManeeshGuptaNvss <48683390+ManeeshGuptaNvss@users.noreply.github.com>
2021-05-30 14:02:52 +0530

    Image Upload config& endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import path from 'path '
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import userRouters from './routes/userRoutes.js'
 import productRouters from './routes/productRoutes.js'
 import orderRouters from './routes/orderRoutes.js'
+import uploadRouters from './routes/uploadRoutes.js'
 dotenv.config()
 
 import connectDB from './config/db.js'
@@ -18,9 +20,16 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRouters)
 app.use('/api/users', userRouters)
 app.use('/api/orders', orderRouters)
+app.use('/api/upload', uploadRouters)
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+// Making a folder static so that it can be accessible and the files inside it get loaded in the browser
+// ES module doesn't have dirname so we mimic that with path.resolve method
+// Only node modules has the constant __dirname
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use(notFound)
 app.use(errorHandler)
 const PORT = process.env.PORT || 5000
